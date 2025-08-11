ff4229df3f4dcec18a959f5cc02e30af37009945
harundarat <harundarat@gmail.com>
2025-08-11 23:10:08 +0700

    ServeMux

--- server_test.go
+++ server_test.go
@@ -1,6 +1,7 @@
 package golang_web
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 )
@@ -10,6 +11,30 @@ func TestServer(t *testing.T) {
 		Addr: "localhost:3000",
 	}
 
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestServeMux(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Hello World")
+	})
+	mux.HandleFunc("/hi", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Hi")
+	})
+	mux.HandleFunc("/images/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Images")
+	})
+	mux.HandleFunc("/images/thumbnails", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Thumbnails")
+	})
+	server := http.Server{
+		Addr:    "localhost:3000",
+		Handler: mux,
+	}
 	err := server.ListenAndServe()
 	if err != nil {
 		panic(err)
